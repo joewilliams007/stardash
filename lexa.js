bea1272a022d4f9e7a24c5195037ecb4c10921c5
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-12-18 09:47:06 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -3196,7 +3196,7 @@ fs.readFile(`./data/users/${sender.split("@")[0]}/xp.json`, 'utf-8', function(er
 	})
 })
 await delay(1000) /// waiting 1 second.		
-if (((Number(xp) > Number(winner1)) && (Number(xp) > Number(winner2)) && (Number(xp) > Number(winner3))))  {						
+if ((Number(xp) > Number(winner1)) && (Number(xp) > Number(winner2)) && (Number(xp) > Number(winner3)))  {						
     exec(`rm -rf ./session/winner1.json`)
     await delay(2000) /// waiting 1 second.
 fs.appendFile(`./session/winner1.json`, `["${xp}", "${pushname}", "${status}"]`, function (err) {				
@@ -3204,7 +3204,7 @@ fs.appendFile(`./session/winner1.json`, `["${xp}", "${pushname}", "${status}"]`,
   });
 }
 
-else if ((Number(xp) > Number(winner2)) && (Number(xp) > Number(winner3))) {						
+else if ((Number(xp) > Number(winner2) && (Number(xp) > Number(winner3)) {						
     exec(`rm -rf ./session/winner2.json`)
     await delay(2000) /// waiting 1 second.
 fs.appendFile(`./session/winner2.json`, `["${xp}", "${pushname}", "${status}"]`, function (err) {				
@@ -3212,7 +3212,7 @@ fs.appendFile(`./session/winner2.json`, `["${xp}", "${pushname}", "${status}"]`,
   });
 }
 
-else if ((Number(xp) > Number(winner3)))  {						
+else if (Number(xp) > Number(winner3))  {						
     exec(`rm -rf ./session/winner3.json`)
     await delay(2000) /// waiting 1 second.
 fs.appendFile(`./session/winner3.json`, `["${xp}", "${pushname}", "${status}"]`, function (err) {				
