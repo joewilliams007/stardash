61646e3ed5a67b3dfcb8c7b59813b953da29f0dd
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-12-22 21:37:11 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -1218,11 +1218,11 @@ const alexa = require("alexa-bot-api-v4");
 const ai = new alexa();
 
 // [] represents context, since it's an array
-ai.getReply(`${design} ${value}`, [], "english", "O_o").then((replys) => {
+ai.getReply(`${value}`, [], "english", "O_o").then((replys) => {
  
-  console.log(replys);
+  console.log(design + " " + replys);
   //Do your stuffs with the reply
-  reply(`${replys}`)
+  reply(`${design} ${replys}`)
 });
 	
 	
