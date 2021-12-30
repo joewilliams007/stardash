2e62b5f3c7315f5af7de36e506f72ca93f406e12
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-12-30 10:29:03 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2818,7 +2818,7 @@ if (!isVerify) return reply(userB())
 	try {
 	var _cmdhit = JSON.parse(fs.readFileSync(`./data/users/${sender.split("@")[0]}/userhit.json`));
 
-			teks = `${design} Your commands\n.reset\nTotal: ${_cmdhit.length}\n\n`
+			teks = `${design} Your commands\nDelete .reset\nTotal: ${_cmdhit.length}\n\n`
 			for (var cmdhit of _cmdhit) {
 			teks += `⑅ ${cmdhit}\n`
 			}
