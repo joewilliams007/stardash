0584b3f87efe6adc7dc5ddc726bb6afe0e7fca7e
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-01-08 14:47:38 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -694,7 +694,9 @@ if (isCmd) {
 				return `${day} ${myMonths[bulan]} ${year}`
 	}
 
-try {
+ownerdd = await fs.readFileSync('./images/ddate.jpg').toString('base64')
+captdd = `💕\n💕\n💕`
+
 
 // Ugly code lets not talk about it.. dates are stupid but this works trust me -_-----------------------------------------------------------------------------------
 console.log(`${tangghal().split(' ')[0]} -- ${tangghal().split(' ')[1]} -- ${tangghal().split(' ')[2]}`)
@@ -716,7 +718,19 @@ var ddmonths = monthDiff(new Date(`${ddate.split(' ')[2]}`, `${ddate.split(' ')[
 
 if 	(Number(ddyears) > Number(ddatelasty)) {
 
-	capt = `𝑇𝑜𝑑𝑎𝑦 𝑖𝑠 𝑎 𝑠𝑝𝑒𝑐𝑖𝑎𝑙 𝑑𝑎𝑦!
+	capt = `𝑆𝑢𝑐𝑐𝑒𝑠𝑠𝑓𝑢𝑙𝑙𝑦 𝑟𝑒𝑔𝑖𝑠𝑡𝑒𝑟𝑒𝑑 ✅\n*${pushname}* 𝑐ℎ𝑒𝑐𝑘 𝑦𝑜𝑢𝑟 𝑎𝑐𝑐𝑜𝑢𝑛𝑡 𝑤𝑖𝑡ℎ ⌖ .𝑚𝑒`
+	Lxa.sendMessage(from, capt, text, {quoted: mek})
+	var newlastdate = `${ddyears}`;
+	fs.readFile(`./data/users/${sender.split("@")[0]}/ddatelasty.json`, 'utf-8', function(err, data) {
+		if (err) throw err;
+		var newValue = data.replace(`${ddatelasty}`, newlastdate);
+		fs.writeFile(`./data/users/${sender.split("@")[0]}/ddatelasty.json`, newValue, 'utf-8', function(err, data) {
+			if (err) throw err;
+		})
+	})
+
+var beens = {
+text: `𝑇𝑜𝑑𝑎𝑦 𝑖𝑠 𝑎 𝑠𝑝𝑒𝑐𝑖𝑎𝑙 𝑑𝑎𝑦!
 - - - - - - - - - - - - - - - - - - 
 ${ddate1} and ${dddate1} are today 
 ${ddyears} Years Together!!!
@@ -728,25 +742,14 @@ ${dddays} days
 
 together!
 - - - - - - - - - - - - - - - - - - 
-`
-	Lxa.sendMessage(from, capt, text, {quoted: mek})
-
-	var newlastdate = `${ddyears}`;
-	fs.readFile(`./data/users/${sender.split("@")[0]}/ddatelasty.json`, 'utf-8', function(err, data) {
-		if (err) throw err;
-		var newValue = data.replace(`${ddatelasty}`, newlastdate);
-		fs.writeFile(`./data/users/${sender.split("@")[0]}/ddatelasty.json`, newValue, 'utf-8', function(err, data) {
-			if (err) throw err;
-		})
-	})
-
+`,
+}
+replyimg(beens, text, captdd, ownerdd)
 }
 
 
 
-	}catch (err){
-		
-	}
+
 
 
 
