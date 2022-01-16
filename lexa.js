502ae3794f6f5e4de4628a02920f8d67764b36d8
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-01-16 19:06:43 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2802,13 +2802,15 @@ const cli = new imdb.Client({apiKey: 'fbc0659b'});
 cli.search({'name': `${value}`}).then((search) => {
   for (const result of search.results) {
     console.log(result);
+	
+	reply(`${design} Movie
+	- - - - - - - - - - - - - - - - - -
+	_${result[0].title}_
+	- - - - - - - - - - - - - - - - - -`)
 
 }
 
-	reply(`${design} Movie
-- - - - - - - - - - - - - - - - - -
-_${result[0].title}_
-- - - - - - - - - - - - - - - - - -`)
+
   
 });
 
