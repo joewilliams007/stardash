fb032a52f1a2d3fcdf3a330e511f80459a06e151
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-01-16 21:47:39 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -1980,7 +1980,7 @@ break
 //-- Qr code
 case 'qrcode':
 case 'qr':
-
+if (args.length < 1) return reply(`${design} Enter Word :)`)	
 request = require('request'); 
  var download = function(uri, filename, callback){ request.head(uri, function(err, res, body){ 
  console.log('content-type:', res.headers['content-type']); 
@@ -1989,10 +1989,10 @@ request = require('request');
  }); 
  
  };
- download(`http://api.qrserver.com/v1/create-qr-code/?data=${value}&size=100x100`, 'output.jpg', function(){ 
+ download(`http://api.qrserver.com/v1/create-qr-code/?data=${args[0]}&size=100x100`, 'output.jpg', function(){ 
  console.log('done'); 
  					buffer = fs.readFileSync('output.jpg')
-						Lxa.sendMessage(from, buffer, image, {quoted:mek, caption: `${design} Your personal QR code\n- - - - - - - - - - - - - - - - - -\n✅ :)`})
+						Lxa.sendMessage(from, buffer, image, {quoted:mek, caption: `${design} Your personal QR code with the text ${args[0]}\n- - - - - - - - - - - - - - - - - -\n✅ :)`})
 });
 
 break
