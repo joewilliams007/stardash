c77bf513257f1b675e412a1e28108278ab304442
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-01-16 18:52:23 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2788,6 +2788,21 @@ ${res}`)
 			})
 		})
 break
+//-- Movie
+case 'movie':
+case 'film':
+	if (!isVerify) return reply(userB())
+	if (args.length < 1) return reply(`${design} What Movie do you want to know about?`) 
+
+const imdb = require('imdb-api')
+imdb.get({name: `${value}`}, {apiKey: 'fbc0659b', timeout: 30000}).then(console.log).catch(console.log);
+
+reply(`${design} Definition
+- - - - - - - - - - - - - - - - - -
+_${idmb.get}_
+- - - - - - - - - - - - - - - - - -`)
+
+break
 //-- Define
 case 'define':
 case 'whats':
